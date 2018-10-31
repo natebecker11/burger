2e4eacc96de3be63ac3384fa703ecdaa9e575c50
natebecker11 <natebecker11@gmail.com>
2018-10-31 16:39:31 -0400

    configured for jaws_db

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'tiggerTiger1',
-  database: 'burgers_db'
-})
+const connection = process.env.JAWSDB_URL ? 
+  mysql.createConnection(process.env.JAWSDB_URL) : 
+  mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'tiggerTiger1',
+    database: 'burgers_db'
+  })
+
 
 connection.connect(err => {
   if (err) {
--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 3000
+const app = express()
+const exphbs = require('express-handlebars')
+const routes = require('./controllers/burgers_controller.js')
+
+app.use(express.static('public'))
+  .use(bodyParser.urlencoded({ extended: true}))
+  .use(bodyParser.json())
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+  .set('view engine', 'handlebars')
+  .use(routes)
+
+app.listen(PORT, () => {
+  console.log('Server listening on: http://localhost:' + PORT)
+})
+
+
