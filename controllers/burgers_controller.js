f6fc82ba309f1862c723a3c5f2facd67d57f4a5b
natebecker11 <natebecker11@gmail.com>
2018-11-01 18:09:03 -0400

    GET route done

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,19 @@ const express = require('express')
 const router = express.Router()
 const burger = require('../models/burger.js')
 
+router.get('/', (req, res) => {
+  burger.selectAll((data) => {
+    console.log(data)
+    let burgers = {
+      data: data
+    }
+    res.render('index', burgers)
+  })  
+})
 
+// router.post('/api/burgers', (req, res) => {
+//   burger.insertOne(())
+// })
 
 
 
