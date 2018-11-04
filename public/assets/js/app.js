fe4eb4bb52967d139828fcfabefcc47605a776c4
natebecker11 <natebecker11@gmail.com>
2018-11-04 17:55:44 -0500

    responsiveness

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,10 +1,19 @@
 $('#burgerSubmit').on('click', event => {
-  // event.preventDefault()
-  let newBurgerName = $('#burgerInput').val()
+  event.preventDefault()
+  let newBurgerName = $('#burgerInput').val().trim()
   let payload = {
     burgerName: newBurgerName
   }
-  $.post('/api/burgers', payload)
+  if (!newBurgerName || newBurgerName.length >= 50) {
+    $('.form-alert').removeClass('invis')
+    $('#burgerInput').val('')
+  } else {
+    $.post('/api/burgers', payload)
+  }
+})
+
+$('#burgerInput').on('click', event => {
+  $('.form-alert').addClass('invis')
 })
 
 $('.devourBtn').on('click', event => {
@@ -20,4 +29,14 @@ $('.devourBtn').on('click', event => {
     method: 'PUT',
     complete: () => location.reload()
   })
+})
+
+// Listener to show/hide burger content areas on button click
+$('#burgerButtons button').on('click', (event) => {
+  event.preventDefault()
+  let shownDiv = `#${$(event.target).attr('data-id')}Div`
+  $('.burger-content').css('display', 'none')
+  $(shownDiv).css('display', 'block')
+  $('#burgerButtons button').removeClass('active')
+  $(event.target).addClass('active')
 })
